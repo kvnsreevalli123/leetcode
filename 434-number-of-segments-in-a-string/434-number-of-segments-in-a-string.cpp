33988629811c634a993848f5fa817cab453ffa3b
Kanamarlapudi Venkata Naga Sreevalli <66547311+kvnsreevalli123@users.noreply.github.com>
2022-08-07 20:28:08 +0530

    Time: 2 ms (41.98%), Space: 6.3 MB (8.11%) - LeetHub

---
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countSegments(string s) {
+     int words=0;
+        int c=1;
+        string s1=" ";
+        vector<string> a;
+       for(int i=0;i<s.length();i++)
+       {
+           if(s[i]!=' ')
+           {
+              s1=s1+s[i];
+               c=0;
+           }
+           if(c==0 && s[i]==' ')
+           {
+               a.push_back(s1);
+               s1=" ";
+               c=1;
+           }
+           if(c==0 && i==s.length()-1)
+           {a.push_back(s1);
+               s1=" ";
+               c=1;
+               
+           }
+           
+       }
+
+       return a.size(); 
+    }
+};
